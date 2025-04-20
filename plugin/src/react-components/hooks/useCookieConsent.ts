e93d5a614ae046164d1d3db6f37ad3492400a777
Michael Vuolo <michaelv@tilli.pro>
2025-04-20 02:59:08 -0400

    refactor: simplify cookie consent initialization by directly importing run function and removing unnecessary declarations

--- plugin/src/react-components/hooks/useCookieConsent.ts
+++ plugin/src/react-components/hooks/useCookieConsent.ts
@@ -2,23 +2,11 @@
 
 import { useEffect } from "react";
 
-import type * as _CookieConsent from "@tilli-pro/cookieconsent";
+import { run } from "@tilli-pro/cookieconsent";
 
 import { COOKIE_CONSENT_ENABLED } from "../../_consts";
 import config from "../../config";
 import { makeInitFn } from "../../init/utils";
-
-declare const CookieConsent:
-  | {
-      run?: typeof _CookieConsent.run;
-    }
-  | undefined;
-
-export const run =
-  typeof CookieConsent !== "undefined" &&
-  typeof CookieConsent.run === "function"
-    ? CookieConsent.run
-    : (await import("@tilli-pro/cookieconsent")).run;
 
 export default function useCookieConsent() {
   useEffect(() => {
