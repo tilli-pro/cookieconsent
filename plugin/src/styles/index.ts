d6b437da5710e7593d1a60b4089c5f7c61e48370
Michael Vuolo <michaelv@tilli.pro>
2025-04-19 22:12:06 -0400

    chore: remove demo HTML files for freeman and frontier tenants and update styles index

--- plugin/src/styles/index.ts
+++ plugin/src/styles/index.ts
@@ -1,4 +1,7 @@
 export default {
+  "tilli-website": "tilli-website.css",
+  "tilli-pay": "tilli-pay.css",
+  nudge: "nudge.css",
   tenants: {
     freeman: "freeman.css",
     frontier: "frontier.css",
