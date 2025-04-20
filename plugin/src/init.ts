fa27f586f489b439317b276755c06c04e875d3b4
Michael Vuolo <michaelv@tilli.pro>
2025-04-20 02:11:09 -0400

    chore: update CDN links to version 778bb6a598fe86f50ce617ea9df80a8b23fd7a35 and bump version to 0.7.31 for cookie consent plugin

--- plugin/src/init.ts
+++ plugin/src/init.ts
@@ -1,4 +1,4 @@
-import "https://cdn.jsdelivr.net/gh/tilli-pro/cookieconsent@e7ce1cf75b91068c10b64cbaa7fff5020fe90880/dist/cookieconsent.umd.js";
+import "https://cdn.jsdelivr.net/gh/tilli-pro/cookieconsent@778bb6a598fe86f50ce617ea9df80a8b23fd7a35/dist/cookieconsent.umd.js";
 
 import type * as _CookieConsent from "@tilli-pro/cookieconsent";
 import type { CookieConsentConfig } from "@tilli-pro/cookieconsent";
@@ -18,7 +18,7 @@ declare global {
   }
 }
 
-const GIT_SHA = "e7ce1cf75b91068c10b64cbaa7fff5020fe90880";
+const GIT_SHA = "778bb6a598fe86f50ce617ea9df80a8b23fd7a35";
 const GIT_REPO = "tilli-pro/cookieconsent";
 const GIT_CDN_BASE_URL = "https://cdn.jsdelivr.net/gh";
 const GIT_CDN_URL = `${GIT_CDN_BASE_URL}/${GIT_REPO}@${GIT_SHA}`;
