53b3a79c8df008bb8587c7a9258f4939e87ffe11
Michael Vuolo <michaelv@tilli.pro>
2025-04-20 02:18:52 -0400

    chore: update CDN links to version fa27f586f489b439317b276755c06c04e875d3b4, enhance font weight for con-edison and oru styles, and bump version to 0.7.33 for cookie consent plugin

--- dist/plugin/init.js
+++ dist/plugin/init.js
@@ -1,9 +1,9 @@
-import "https://cdn.jsdelivr.net/gh/tilli-pro/cookieconsent@778bb6a598fe86f50ce617ea9df80a8b23fd7a35/dist/cookieconsent.umd.js";
+import "https://cdn.jsdelivr.net/gh/tilli-pro/cookieconsent@fa27f586f489b439317b276755c06c04e875d3b4/dist/cookieconsent.umd.js";
 import { cookieConsentTheme } from "./_utils.js";
 import _config from "./config/index.js";
 import styles from "./styles/index.js";
 import { initTheme } from "./init/utils.script.js";
-const GIT_SHA = "778bb6a598fe86f50ce617ea9df80a8b23fd7a35";
+const GIT_SHA = "fa27f586f489b439317b276755c06c04e875d3b4";
 const GIT_REPO = "tilli-pro/cookieconsent";
 const GIT_CDN_BASE_URL = "https://cdn.jsdelivr.net/gh";
 const GIT_CDN_URL = `${GIT_CDN_BASE_URL}/${GIT_REPO}@${GIT_SHA}`;
