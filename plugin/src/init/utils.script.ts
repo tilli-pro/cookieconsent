0bf9fe7d398ffe228f11e91c5d3f23f0479ff663
Michael Vuolo <michaelv@tilli.pro>
2025-04-20 00:40:16 -0400

    chore: remove debug logging from addHtmlClass and initTheme functions

--- plugin/src/init/utils.script.ts
+++ plugin/src/init/utils.script.ts
@@ -8,12 +8,8 @@ export function getThemeFromScriptUrl(scriptUrl: string): string | null {
 export function addHtmlClass(theme: string): void {
   const className = `cc--${theme}`;
   const htmlEl = document.documentElement;
-  console.debug({ className }, "[cc-plugin]: addHtmlClass");
-  console.debug({ htmlEl }, "[cc-plugin]: addHtmlClass");
 
   if (!htmlEl.classList.contains(className)) htmlEl.classList.add(className);
-
-  console.debug({ classList: htmlEl.classList }, "[cc-plugin]: addHtmlClass");
 }
 
 /**
@@ -52,10 +48,8 @@ export function getCurrentScriptUrl(): string | undefined {
  */
 export function initTheme(scriptUrl?: string): void {
   const url = scriptUrl ?? getCurrentScriptUrl();
-  console.debug({ url }, "[cc-plugin]: initTheme");
   if (!url) return;
 
   const theme = getThemeFromScriptUrl(url);
-  console.debug({ theme }, "[cc-plugin]: initTheme");
   if (theme) addHtmlClass(theme);
 }
