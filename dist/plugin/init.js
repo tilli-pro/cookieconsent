ff2a39b7e56435ad7584df3fba22cdc578fd7e7d
Michael Vuolo <michaelv@tilli.pro>
2025-04-20 02:07:58 -0400

    chore: update CDN links to version e7ce1cf75b91068c10b64cbaa7fff5020fe90880 and bump version to 0.7.29 for cookie consent plugin

--- dist/plugin/init.js
+++ dist/plugin/init.js
@@ -1,9 +1,9 @@
-import "https://cdn.jsdelivr.net/gh/tilli-pro/cookieconsent@b6cde5db0f14c4f61a5a15fd9b81802975d3d987/dist/cookieconsent.umd.js";
+import "https://cdn.jsdelivr.net/gh/tilli-pro/cookieconsent@e7ce1cf75b91068c10b64cbaa7fff5020fe90880/dist/cookieconsent.umd.js";
 import { cookieConsentTheme } from "./_utils.js";
 import _config from "./config/index.js";
 import styles from "./styles/index.js";
 import { initTheme } from "./init/utils.script.js";
-const GIT_SHA = "b6cde5db0f14c4f61a5a15fd9b81802975d3d987";
+const GIT_SHA = "e7ce1cf75b91068c10b64cbaa7fff5020fe90880";
 const GIT_REPO = "tilli-pro/cookieconsent";
 const GIT_CDN_BASE_URL = "https://cdn.jsdelivr.net/gh";
 const GIT_CDN_URL = `${GIT_CDN_BASE_URL}/${GIT_REPO}@${GIT_SHA}`;
--- plugin/src/init.ts
+++ plugin/src/init.ts
@@ -1,4 +1,4 @@
-import "https://cdn.jsdelivr.net/gh/tilli-pro/cookieconsent@b6cde5db0f14c4f61a5a15fd9b81802975d3d987/dist/cookieconsent.umd.js";
+import "https://cdn.jsdelivr.net/gh/tilli-pro/cookieconsent@e7ce1cf75b91068c10b64cbaa7fff5020fe90880/dist/cookieconsent.umd.js";
 
 import type * as _CookieConsent from "@tilli-pro/cookieconsent";
 import type { CookieConsentConfig } from "@tilli-pro/cookieconsent";
@@ -18,7 +18,7 @@ declare global {
   }
 }
 
-const GIT_SHA = "b6cde5db0f14c4f61a5a15fd9b81802975d3d987";
+const GIT_SHA = "e7ce1cf75b91068c10b64cbaa7fff5020fe90880";
 const GIT_REPO = "tilli-pro/cookieconsent";
 const GIT_CDN_BASE_URL = "https://cdn.jsdelivr.net/gh";
 const GIT_CDN_URL = `${GIT_CDN_BASE_URL}/${GIT_REPO}@${GIT_SHA}`;
