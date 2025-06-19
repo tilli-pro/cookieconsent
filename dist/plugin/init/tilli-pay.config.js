dd31957f9ae95bb46f53594bf4d1d284c6e3c02c
Michael Vuolo <michaelv@tilli.pro>
2025-06-19 10:35:43 -0400

    feat(plugin): add datadog analytics for tilli pay config

--- dist/plugin/init/tilli-pay.config.js
+++ dist/plugin/init/tilli-pay.config.js
@@ -1,9 +1,29 @@
 import _config from "../config/index.js";
 import { stripInvalidLinkedCategoriesFromTranslations } from "./utils.js";
+import { LABELS } from "../config/categories/labels.js";
+import cookies from "../config/cookies/index.js";
+// TODO: auto-detect detect language
+const __LANGUAGE__ = "en"; // "English" ("English")
 const categories = {
     necessary: {
         enabled: true, // "necessary" category is always enabled.
         readOnly: true,
+    },
+    analytics: {
+        services: {
+            /** Datadog (RUM) */
+            datadog: {
+                label: LABELS.en.analytics.datadog,
+                cookies: [...cookies.analytics.datadog(__LANGUAGE__)],
+            },
+        },
+        autoClear: {
+            cookies: [
+                {
+                    name: /^(_tilli_analytics_dd_)/, // Datadog (RUM)
+                },
+            ],
+        },
     },
 };
 // TODO: DRY further w/ `./brf.ts`
--- plugin/src/init/tilli-pay.config.ts
+++ plugin/src/init/tilli-pay.config.ts
@@ -2,11 +2,32 @@ import type { CookieConsentConfig } from "@tilli-pro/cookieconsent";
 
 import _config from "../config";
 import { stripInvalidLinkedCategoriesFromTranslations } from "./utils";
+import { LABELS } from "../config/categories/labels";
+import cookies from "../config/cookies";
+
+// TODO: auto-detect detect language
+const __LANGUAGE__ = "en"; // "English" ("English")
 
 const categories: CookieConsentConfig["categories"] = {
   necessary: {
     enabled: true, // "necessary" category is always enabled.
     readOnly: true,
+  },
+  analytics: {
+    services: {
+      /** Datadog (RUM) */
+      datadog: {
+        label: LABELS.en.analytics.datadog,
+        cookies: [...cookies.analytics.datadog(__LANGUAGE__)],
+      },
+    },
+    autoClear: {
+      cookies: [
+        {
+          name: /^(_tilli_analytics_dd_)/, // Datadog (RUM)
+        },
+      ],
+    },
   },
 };
 
