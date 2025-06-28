ee6aef4ba93efda5e49b18001fb5bc399cdbc0fe
Michael Vuolo <michaelv@tilli.pro>
2025-06-28 14:16:00 -0400

    fix(cookiePrefsButtonDragObserver): remove passive option from event listeners for improved drag functionality

--- plugin/src/config/gui-options/scripts/cookiePrefsButtonDragObserver.ts
+++ plugin/src/config/gui-options/scripts/cookiePrefsButtonDragObserver.ts
@@ -108,13 +108,13 @@ export default (function <IsTouch extends boolean = false>(
     highestBottomPx = originalBottomPx + maxDragPx;
 
     /** attach the mouse events for dragging */
-    buttonEl.addEventListener("mousedown", onMouseDown.bind(buttonEl), { passive: true });
-    document.addEventListener("mousemove", onMouseMove.bind(buttonEl), { passive: true });
-    document.addEventListener("mouseup", onMouseUp, { passive: true });
+    buttonEl.addEventListener("mousedown", onMouseDown.bind(buttonEl));
+    document.addEventListener("mousemove", onMouseMove.bind(buttonEl));
+    document.addEventListener("mouseup", onMouseUp);
     // [mobile (touch) support]
-    buttonEl.addEventListener("touchstart", onTouchStart.bind(buttonEl), { passive: true });
-    document.addEventListener("touchmove", onTouchMove.bind(buttonEl), { passive: true });
-    document.addEventListener("touchend", onTouchEnd, { passive: true });
+    buttonEl.addEventListener("touchstart", onTouchStart.bind(buttonEl));
+    document.addEventListener("touchmove", onTouchMove.bind(buttonEl));
+    document.addEventListener("touchend", onTouchEnd);
   }
 
   // MUTATION OBSERVER – for if the button is removed/re-added
