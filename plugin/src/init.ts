c0febaf337f46289f1afec9a251d062049f73ca8
Michael Vuolo <michaelv@tilli.pro>
2025-06-28 14:50:11 -0400

    chore: update git sha

--- plugin/src/init.ts
+++ plugin/src/init.ts
@@ -1,4 +1,4 @@
-import "https://cdn.jsdelivr.net/gh/tilli-pro/cookieconsent@bc9461c70959e0fe9264c871dc18a4d857e65b9a/dist/cookieconsent.umd.js";
+import "https://cdn.jsdelivr.net/gh/tilli-pro/cookieconsent@89802a59f63619636505d49d489ffec021a519c6/dist/cookieconsent.umd.js";
 
 import type * as _CookieConsent from "@tilli-pro/cookieconsent";
 import type { CookieConsentConfig } from "@tilli-pro/cookieconsent";
@@ -18,7 +18,7 @@ declare global {
   }
 }
 
-const GIT_SHA = "bc9461c70959e0fe9264c871dc18a4d857e65b9a";
+const GIT_SHA = "89802a59f63619636505d49d489ffec021a519c6";
 const GIT_REPO = "tilli-pro/cookieconsent";
 const GIT_CDN_BASE_URL = "https://cdn.jsdelivr.net/gh";
 const GIT_CDN_URL = `${GIT_CDN_BASE_URL}/${GIT_REPO}@${GIT_SHA}`;
